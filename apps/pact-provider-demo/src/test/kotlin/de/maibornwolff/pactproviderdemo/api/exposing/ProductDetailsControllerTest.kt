e79050ffa5da901a2d098d658c221c23afa697bc
Alexander Henze <alexander.henze@maibornwolff.de>
2019-07-25 12:16:37 +0200

    initial commit

--- apps/pact-provider-demo/src/test/kotlin/de/maibornwolff/pactproviderdemo/api/exposing/ProductDetailsControllerTest.kt
+++ apps/pact-provider-demo/src/test/kotlin/de/maibornwolff/pactproviderdemo/api/exposing/ProductDetailsControllerTest.kt
@@ -1,38 +1,37 @@
 package de.maibornwolff.pactproviderdemo.api.exposing
 
-import au.com.dius.pact.provider.junit.Provider
-import au.com.dius.pact.provider.junit.State
-import au.com.dius.pact.provider.junit.loader.PactBroker
-import au.com.dius.pact.provider.spring.SpringRestPactRunner
 import de.maibornwolff.pactproviderdemo.entity.ProductDetails
 import de.maibornwolff.pactproviderdemo.entity.ProductDetailsRepository
+import org.hamcrest.CoreMatchers
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
-import au.com.dius.pact.provider.junit.target.HttpTarget
-import au.com.dius.pact.provider.junit.target.TestTarget
-import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget
+import org.springframework.test.web.servlet.get
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
-@RunWith(SpringRestPactRunner::class)
-@Provider("Service-B-provider")
-@PactBroker(host= "localhost")
 internal class ProductDetailsControllerTest {
 
-    @TestTarget
-    val target: SpringBootHttpTarget = SpringBootHttpTarget()
+    @Autowired
+    private lateinit var underTest: MockMvc
 
     @Autowired
     private lateinit var productDetailsRepository: ProductDetailsRepository
 
-    @State("provider has product details for requested product")
-    fun insertProductDetails() {
-        var productDetails = ProductDetails(1L, "Awesome product no 1", 4.99)
+    @Test
+    fun `fetch product details successfully`() {
+        val productId = 1L
+        var productDetails = ProductDetails(productId, "Awesome product no 1", 4.99)
         productDetailsRepository.save(productDetails)
+
+        underTest.get("/productDetails/{id}", productId)
+                .andExpect {
+                    status { `is`(200) }
+                    jsonPath("$.id", CoreMatchers.equalTo(1))
+                    jsonPath("$.name", CoreMatchers.equalTo("Awesome product no 1"))
+                    jsonPath("$.price", CoreMatchers.equalTo(4.99))
+                }
     }
 }
